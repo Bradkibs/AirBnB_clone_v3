6d9621c697024be43b3c650dc8ebd27b4409580a
Bradleykibs <Bradleykibs@gmail.com>
2023-02-17 19:53:08 +0300

    documentation of app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""  runs the web app """
+
+
 import os
 from flask import Flask
 from models import storage
-from api.v1.views import api_views
+from api.v1.views import app_views
 
-app = Flask(__name_)
+
+app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
